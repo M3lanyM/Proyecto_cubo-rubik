ea33e9662b1a2aeb5db994b8d58248cbe267f537
josecastromedi <128188742+josecastromedi@users.noreply.github.com>
2023-09-23 00:15:21 -0600

    add colors to faces

--- RubikCube/src/model/PaneCube.java
+++ RubikCube/src/model/PaneCube.java
@@ -39,27 +39,58 @@ public class PaneCube extends GridPane {
     }
     
     public void whiteFace(){
+        positions.setFront(faces[0]);
+        positions.setBack(faces[1]);
+        positions.setRight(faces[2]);
+        positions.setLeft(faces[3]);
+        positions.setTop(faces[4]);
+        positions.setBottom(faces[5]);
         
     }
     
     public void redFace(){
-        
+        positions.setFront(faces[5]);
+        positions.setBack(faces[4]);
+        positions.setRight(faces[2]);
+        positions.setLeft(faces[3]);
+        positions.setTop(faces[0]);
+        positions.setBottom(faces[1]);
     }
     
     public void yellowFace(){
-        
+        positions.setFront(faces[1]);
+        positions.setBack(faces[0]);
+        positions.setRight(faces[3]);
+        positions.setLeft(faces[2]);
+        positions.setTop(faces[4]);
+        positions.setBottom(faces[5]);
     }
     
     public void blueFace(){
-        
+        positions.setFront(faces[2]);
+        positions.setBack(faces[3]);
+        positions.setRight(faces[1]);
+        positions.setLeft(faces[0]);
+        positions.setTop(faces[4]);
+        positions.setBottom(faces[5]);
     }
     
     public void greenFace(){
-        
+        positions.setFront(faces[3]);
+        positions.setBack(faces[2]);
+        positions.setRight(faces[0]);
+        positions.setLeft(faces[1]);
+        positions.setTop(faces[4]);
+        positions.setBottom(faces[5]);
     }
     
     public void orangeFace(){
-        
+        positions.setFront(faces[4]);
+        positions.setBack(faces[5]);
+        positions.setRight(faces[2]);
+        positions.setLeft(faces[3]);
+        positions.setTop(faces[1]);
+        positions.setBottom(faces[0]);
     }
     
     public void order(){
