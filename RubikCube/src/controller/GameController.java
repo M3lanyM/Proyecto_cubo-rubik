fd1d259dafe6b0f39137e1c695896610c49ae3ab
Melany Mendoza Vargas <melanymendoza2001@gmail.com>
2023-09-26 00:29:24 -0600

    menu design

--- RubikCube/src/controller/GameController.java
+++ RubikCube/src/controller/GameController.java
@@ -9,7 +9,9 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
 
 /**
  * FXML Controller class
@@ -23,13 +25,32 @@ public class GameController implements Initializable {
     @FXML
     private Button btnWhite;
     @FXML
-    private Button btnBlue;
-    @FXML
     private Button btnGreen;
+    @FXML
+    private Button btnBlue;
     @FXML
     private Button btnRed;
     @FXML
     private Button btnOrange;
+    @FXML
+    private RadioButton radioBtn1;
+    @FXML
+    private ToggleGroup rowColumnToggleGroup;
+    @FXML
+    private RadioButton radioBtn2;
+    @FXML
+    private RadioButton radioBtn3;
+    @FXML
+    private RadioButton radioBtn4;
+    @FXML
+    private RadioButton radioBtn5;
+    @FXML
+    private RadioButton radioBtn6;
+    @FXML
+    private StackPane faceCuboBackground;
+    @FXML
+    private Button mixBtn;
+
 
     /**
      * Initializes the controller class.
